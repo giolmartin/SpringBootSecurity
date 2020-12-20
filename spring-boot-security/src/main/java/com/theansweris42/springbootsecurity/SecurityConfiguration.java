17b7d0a6b647cebe836c7379dbb7f82fa04990ec
giolmartin <giovanni.lmartin@gmail.com>
2020-12-20 17:21:18 -0600

    authorization_basics

--- spring-boot-security/src/main/java/com/theansweris42/springbootsecurity/SecurityConfiguration.java
+++ spring-boot-security/src/main/java/com/theansweris42/springbootsecurity/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.theansweris42.springbootsecurity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -23,6 +24,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 							.password("foo")
 							.roles("ADMIN");
 	}
+	
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+					.antMatchers("/admin").hasRole("ADMIN")
+					.antMatchers("/user").hasAnyRole("USER", "ADMIN")
+					.antMatchers("/").permitAll()
+					.and().formLogin();
+	}
+
 
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
