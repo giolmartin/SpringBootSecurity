2ce8e19948fccfbfec61ac2376b6fddfa7624298
giolmartin <giovanni.lmartin@gmail.com>
2020-12-20 17:01:45 -0600

    authentication_basic

---
+++ spring-boot-security/src/main/java/com/theansweris42/springbootsecurity/HomeResource.java
@@ -0,0 +1,13 @@
+package com.theansweris42.springbootsecurity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResource {
+
+	@GetMapping("/")
+	public String home() {
+		return ("<h1> Welcome</h1>");
+	}
+}
---
+++ spring-boot-security/src/test/java/com/theansweris42/springbootsecurity/SpringBootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theansweris42.springbootsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
