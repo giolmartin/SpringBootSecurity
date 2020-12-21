f51a7a31b6046b2a71ef32122b5d56de9c14cfa2
giolmartin <giovanni.lmartin@gmail.com>
2020-12-21 01:54:50 -0600

    first commit

---
+++ spring-security-jpa/src/main/java/com/theansweris42/springsecurityjpa/HomeResource.java
@@ -0,0 +1,30 @@
+package com.theansweris42.springsecurityjpa;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResource {
+
+	Logger log = LoggerFactory.getLogger(this.getClass());
+
+	@GetMapping("/")
+	public String home() {
+		log.info("\"/\" page authorized");
+		return ("<h1> Welcome</h1>");
+	}
+
+	@GetMapping("/user")
+	public String user() {
+		log.info("\"/user\" authorized");
+		return ("<h1>Welcome USER</h1>");
+	}
+
+	@GetMapping("/admin")
+	public String admin() {
+		log.info("\"/admin\" auhtorized");
+		return ("<h1>Welcome ADMIN</h1>");
+	}
+}
---
+++ spring-security-jpa/src/main/java/com/theansweris42/springsecurityjpa/SecurityConfiguration.java
@@ -0,0 +1,29 @@
+package com.theansweris42.springsecurityjpa;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.authorizeRequests()
+				.antMatchers("/admin").hasRole("ADMIN")
+				.antMatchers("/user").hasAnyRole("USER", "ADMIN")
+				.antMatchers("/").permitAll()
+				.and().formLogin();
+	}
+	
+	
+	
+	
+
+}
