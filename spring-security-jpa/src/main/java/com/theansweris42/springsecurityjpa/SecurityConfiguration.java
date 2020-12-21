afd6dad937342f69e772a87828316ff834515967
giolmartin <giovanni.lmartin@gmail.com>
2020-12-21 04:21:58 -0600

    final commit

--- spring-security-jpa/src/main/java/com/theansweris42/springsecurityjpa/SecurityConfiguration.java
+++ spring-security-jpa/src/main/java/com/theansweris42/springsecurityjpa/SecurityConfiguration.java
@@ -1,14 +1,24 @@
 package com.theansweris42.springsecurityjpa;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
+	@Autowired
+	UserDetailsService userDetailsService;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		
+		auth.userDetailsService(userDetailsService);
 		
 	}
 
@@ -22,8 +32,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 				.and().formLogin();
 	}
 	
-	
-	
-	
-
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
 }
---
+++ spring-security-jpa/src/main/java/com/theansweris42/springsecurityjpa/UserRepository.java
@@ -0,0 +1,14 @@
+package com.theansweris42.springsecurityjpa;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.theansweris42.springsecurityjpa.models.User;
+
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+		Optional<User> findByUserName(String userName);
+
+}
